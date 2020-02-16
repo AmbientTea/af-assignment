9ef3cc1ecacec916abcf90b56d3983d46b3dbb03
Nikolaos Dymitriadis <nikolaos.dymitriadis@westwing.pl>
2020-02-16 15:51:42 +0100

    rework test

--- src/test/scala/io/ambienttea/views/stream/WindowedJoinTest.scala
+++ src/test/scala/io/ambienttea/views/stream/WindowedJoinTest.scala
@@ -1,5 +1,6 @@
 package io.ambienttea.views.stream
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source}
 import org.scalatest.Matchers
@@ -9,36 +10,37 @@ class WindowedJoinTest extends AsyncWordSpec with Matchers {
 
   implicit val ac: ActorSystem = ActorSystem()
 
-  "WindowedMerge" should {
-    val source1 = Source(List(1, 2, 3, 4))
-    val source2 = Source(List(1, 2, 5, 6))
+  "WindowedJoin" should {
+    val source: Source[Either[Int, Int], NotUsed] = Source(
+      List(Left(1), Right(2), Left(3), Left(4), Right(5), Right(6))
+    )
 
-    "properly merge two sources" in {
-      val mergedFlow =
-        Pipeline(source1, source2)(identity, identity)(
-          _ * 2,
-          identity,
-          100
-        )
+    "match elements according to the match functions" in {
+      val joinFlow = WindowedJoin[Int, Int, Int, Int](
+        source,
+        (_: Int) * 2,
+        identity(_: Int),
+        maxWindowSize = 100
+      )
       val expectedResult = List(1 -> 2, 3 -> 6)
 
-      mergedFlow.runWith(Sink.seq).map { s =>
+      joinFlow.runWith(Sink.seq).map { s =>
         println(s"$s ?= $expectedResult")
         s should contain theSameElementsInOrderAs expectedResult
       }
     }
 
     "properly merge two sources when window size is smaller than their size" in {
-      val mergedFlow =
-        Pipeline(source1, source2)(identity, identity)(
-          _ * 2,
-          identity,
-          maxWindowSize = 2
-        )
+      val joinFlow = WindowedJoin[Int, Int, Int, Int](
+        source,
+        (_: Int) * 2,
+        identity(_: Int),
+        maxWindowSize = 2
+      )
 
       val expectedResult = List(1 -> 2)
 
-      mergedFlow.runWith(Sink.seq).map { s =>
+      joinFlow.runWith(Sink.seq).map { s =>
         println(s"$s ?= $expectedResult")
         s should contain theSameElementsInOrderAs expectedResult
       }
